a9eb8d7ce8de1efb1ef22019117279aa18e5163b
Eduard Zhereliuk <edw4rdd@gmail.com>
2019-10-15 16:47:14 +0300

    Added positive and negative cases. Surrounded code with try-finally.

--- src/wikipedia_test.py
+++ src/wikipedia_test.py
@@ -1,14 +1,28 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get("https:/wikipedia.org")
+try:
+    driver = webdriver.Chrome()
+    driver.get("https:/wikipedia.org")
 
-search_field = driver.find_element_by_id("searchInput")
-search_button = driver.find_element_by_xpath("//*[@id=\"search-form\"]/fieldset/button/i")
+    search_field = driver.find_element_by_id("searchInput")
+    search_button = driver.find_element_by_xpath("//*[@id=\"search-form\"]/fieldset/button/i")
 
-search_field.send_keys("Test Automation")
-search_button.click()
+    search_text = "Test automation"
+    search_field.send_keys(search_text)
+    search_button.click()
 
-assert "Test automation" in driver.title
+    # Positive test cases
+    # Test browser caption
+    assert search_text in driver.title
 
-driver.quit()
+    # Test article header
+    article_header = driver.find_element_by_xpath("//*[@id=\"firstHeading\"]")
+    assert search_text in article_header.text
+
+    # Negative test case
+    # Test article contents
+    article_contents = driver.find_element_by_class_name("toc")
+    assert "Manual testing" in article_contents.text
+    
+finally:
+    driver.quit()

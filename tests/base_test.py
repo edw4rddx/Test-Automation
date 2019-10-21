72be4c103539559f156cc107ab8a0ddf6a521580
Eduard Zhereliuk <edw4rdd@gmail.com>
2019-10-21 12:15:14 +0300

    pep8 formatting, improved waits

--- tests/base_test.py
+++ tests/base_test.py
@@ -6,6 +6,7 @@ class BaseGoogleAccountCreation(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
 
     def tearDown(self):
         self.driver.close()
